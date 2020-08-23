a88be562f8fc80f772512f9f732e3e24608a25c5
jverzani <jverzani@gmail.com>
2020-08-23 08:07:37 -0400

    test

--- src/SymPyTest.jl
+++ src/SymPyTest.jl
@@ -1,5 +1,22 @@
 module SymPyTest
 
 # Write your package code here.
+using SymPy
+
+const sconst = SymPy.PyCall.PyNULL() # a pyobject
+const sfun = SymPy.PyCall.PyNULL() # a pyobjecdt
+const b =  SymPy.Sym(SymPy.PyCall.PyNULL()); # no show method, so careful
+
+export sconst, sfun, b
+
+
+
+function __init__()
+    copy!(sconst, sympy.pi.__pyobject__)
+    copy!(sfun, sympy.sin)
+    copy!(b.__pyobject__, SymPy.PyCall.PyObject(SymPy.symbols("b")))
+end
+
+
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,10 @@ using SymPyTest
 using Test
 
 @testset "SymPyTest.jl" begin
-    # Write your tests here.
+
+    @test SymPyTest.SymPy.Sym(sconst) == SymPyTest.SymPy.PI
+    @test sfun(sconst) == 0
+    @test SymPyTest.SymPy.factor(b^2 - 1)  == (b-1)*(b+1)
+
+    
 end
